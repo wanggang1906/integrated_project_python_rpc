fde9bf9a0e5a7577f06dea7a732757b89726e9ec
wanggang1906 <1294901911@qq.com>
2020-04-28 23:20:11 +0800

    r基础

--- r_test/r_test03.R
+++ r_test/r_test03.R
@@ -3,15 +3,18 @@
 # Created by: wang gang
 # Created on: 2020/4/25
 
-library(ggplot2)
-rm(list = ls())
-dat <- data.frame(
+Sys.setlocale("LC_ALL","Chinese") # 设置中文字符集---但是没用
+library(ggplot2) # 加载包
+rm(list = ls()) # 清空变量
+dat <- data.frame( # 赋值
   time = factor(c("Lunch","Dinner"), levels=c("Lunch","Dinner")),
   total_bill = c(14.89, 17.23)
 )
-dat
+print("dat数据为：")
 print(dat)
+
 ggplot(data=dat, aes(x=time, y=total_bill, fill=time)) +
-    geom_bar(stat="identity")
+geom_bar(stat="identity")
 
 print(ggplot)
+
